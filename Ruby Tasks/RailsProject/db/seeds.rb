1ce0e0900a074e35f9ce271c26e4351687c865dc
imposttor <i.v.metelev99@gmail.com>
2021-10-29 16:33:46 +0600

    add find_or_create in seeds cycle

--- Ruby Tasks/RailsProject/db/seeds.rb
+++ Ruby Tasks/RailsProject/db/seeds.rb
@@ -1,10 +1,10 @@
 #Author.delete_all
 #Book.delete_all
 10.times do |x|
-    Author.create({id: x, name: "Author #{x}"})
+    Author.find_or_create_by({id: x, name: "Author #{x}"})
 end
 10.times do |y|
     10.times do |x|
-        Book.create({id: y*10 + x, author_id: y, name: "Book #{x}"})
+        Book.find_or_create_by({id: y*10 + x, author_id: y, name: "Book #{x}"})
     end
 end
